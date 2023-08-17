808d8616b5898eca402265d09761f6a3a1b71574
esquilo korte <dudu.korte@gmail.com>
2023-08-17 10:56:36 -0300

    Create debug.java

---
+++ global_classes/debug.java
@@ -0,0 +1,9 @@
+package global_classes;
+
+public class debug {
+    public static void main(String[] args) {
+        pessoas teste = new pessoas("teste2", 15, "kdjfkjsdf", "dnfjksdnf", "kdsflsjd", "ksdjfkdfsjlg");
+        System.out.println(teste.nome);
+        System.out.println(teste.idade);
+    }
+}
