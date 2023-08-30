812b71f875e03c90b7d19942ac51aa2dac7f82e3
esquilo korte <dudu.korte@gmail.com>
2023-08-30 20:09:56 -0300

    Delete global_classes directory

--- global_classes/debug.java
+++
@@ -1,9 +0,0 @@
-package global_classes;
-
-public class debug {
-    public static void main(String[] args) {
-        pessoas teste = new pessoas("teste2", 15, "kdjfkjsdf", "dnfjksdnf", "kdsflsjd", "ksdjfkdfsjlg");
-        System.out.println(teste.nome);
-        System.out.println(teste.idade);
-    }
-}
